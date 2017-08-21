8ddcd569b2085791799edf7cf07f5d4c4e90023c
Eric Sharkey <ers16e@my.fsu.edu>
2017-08-21 01:12:10 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const token = "MzQ4ODczNTY0MDE3MDAwNDQ5.DHtRVg.9bjhDbkdY7zhdOPAUGC_BiJirdM";
+
+var isReady = true;
+
+client.on('ready', () => {
+    console.log('I am ready');
+});
+
+client.on('message', message => {
+    if(isReady && message.content.startsWith('=')){
+        
+        var number = message.content.substr(1);
+        if(parseInt(number) > 0 && parseInt(number) <= 100)
+        {
+            
+            var voiceChannel = message.member.voiceChannel;
+            if (typeof voiceChannel != 'undefined')
+            {
+                isReady = false;
+                voiceChannel.join().then(connection => {
+                var filePath = './sounds/' + number + '.wav';
+                const dispatcher = connection.playFile(filePath);
+                dispatcher.on("end", end => {
+                    voiceChannel.leave();
+                    isReady = true;
+                });
+                }).catch(err => console.log(err));
+            }
+            
+        }
+        
+    }
+});
+
+client.login(token);
