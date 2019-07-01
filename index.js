ec432735722dacb45c24bfff24f664c0454f5a18
Eric Sharkey <ers16e@my.fsu.edu>
2019-07-01 19:02:15 -0400

    Added promise catches

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-const client = new Discord.Client();
+const client = new Discord.Client({autoReconnect:true});
 fs = require('fs');
 const token = fs.readFileSync('token.txt', 'utf8');
 
@@ -12,12 +12,13 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if(isReady && message.content.startsWith('=')){
+    if(isReady && message.content.startsWith('=') && message.content.length > 1){
         
         var number = message.content.substr(1);
         if(parseInt(number) > 0 && parseInt(number) <= 100)
         {
-            message.delete(1000);
+            message.delete(1000).then(message => console.log('Deleted message from ${message.author.username}'))
+                                .catch(console.error);
             var voiceChannel = message.member.voiceChannel;
             if (typeof voiceChannel != 'undefined')
             {
